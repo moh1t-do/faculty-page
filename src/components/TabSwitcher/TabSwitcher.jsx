d0081c97f1a2ae12222b7154678e75b2549088ff
Avyukt <avyuktrkumar08@gmail.com>
2023-10-07 22:24:39 +0530

    test

--- src/components/TabSwitcher/TabSwitcher.jsx
+++ src/components/TabSwitcher/TabSwitcher.jsx
@@ -3,24 +3,37 @@ import "./TabSwitcher.css"
 import Pending from '../Pending/Pending'
 import Approved from '../Approved/Approved'
 import Rejected from '../Rejected/Rejected'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck, faCircleXmark, faHourglassHalf } from '@fortawesome/free-solid-svg-icons'
 
 function TabSwitcher() {
 
   const [tab, setTab]=useState(1);
   console.log(tab);
   return (
-    <div class="content">
+    <div className="first">
+      <div class="content">
         <h2 class="text-center">Approval Page</h2>
         <div class="carousel-nav">
-          <a href="#" className={tab===1?"col active":"col"} onClick={() => setTab(1)}>Pending</a>
-          <a href="#" className={tab===2?"col active":"col"} onClick={() => setTab(2)}>Approved</a>
-          <a href="#" className={tab===3?"col active":"col"} onClick={() => setTab(3)}>Rejected</a>
+          <a href="#" className={tab===1?"col active":"col"} onClick={() => setTab(1)}>
+            <FontAwesomeIcon className='tab-icon' icon={faHourglassHalf} />
+            Pending
+          </a>
+          <a href="#" className={tab===2?"col active active2":"col"} onClick={() => setTab(2)}>
+            <FontAwesomeIcon className='tab-icon' icon={faCircleCheck} />
+            Approved
+          </a>
+          <a href="#" className={tab===3?"col active active3":"col"} onClick={() => setTab(3)}>
+            <FontAwesomeIcon className='tab-icon' icon={faCircleXmark} />
+            Rejected
+          </a>
         </div>
         <div>
           {tab===1 && <Pending/>}
           {tab===2 && <Approved/>}
           {tab===3 && <Rejected/>}
         </div>
+      </div>
     </div>
   )
 }
