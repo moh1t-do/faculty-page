ef48bb112cc43df007a53b212a4d05ecc7f3d25d
Avyukt <avyuktrkumar08@gmail.com>
2023-10-07 11:09:08 +0530

    test

---
+++ src/components/Approved/Approved.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../TabSwitcher/TabSwitcher.css"
+
+function Approved() {
+  return (
+    <div class="tab">
+        <div class="text">
+            <div class="category">Second Tab</div>
+        </div>
+    </div>
+  )
+}
+
+export default Approved
